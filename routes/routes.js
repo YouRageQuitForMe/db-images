a786ac690a9b38586c0045ef1efe4b226d6f6f00
YouRageQuitForMe <youragequitforxd@gmail.com>
2022-02-01 20:21:39 +0100

    setup

---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const { handlePost } = require("../controllers/controller");
+
+router.post('/post-image', (req, res) => {
+
+})
+
+module.exports = router;
